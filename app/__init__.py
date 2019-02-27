8747247bd068b1e86ee75f75fc5a8deadf39fd24
chrisribia <chrisribia@gmail.com>
2019-02-27 20:20:45 +0300

    add travis

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 
 from flask import Blueprint, Flask 
-
+from app.api.v1.routes import VERSION_ONE as v1
 
 from app.instance.config import APP_CONFIG 
 
@@ -11,6 +11,8 @@ def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(APP_CONFIG[config_name])# Load the default configuration
     app.url_map.strict_slashes = False
+
+    app.register_blueprint(v1)
    
  
     
